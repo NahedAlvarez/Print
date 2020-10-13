c9f02cc1af03dd2f135d8ccca3dcc621791891d4
Nahed David <nahed.alvarez@timba.co>
2020-10-13 17:55:27 -0500

    commit inicial

---
+++ Assets/Scripts/Print.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SmartDLL;
+using UnityEngine.UI;
+
+public class Print : MonoBehaviour
+{
+    public SmartPrinter smartPrinter = new SmartPrinter();
+
+    public InputField inputField;
+    public Button printButton;
+    string headerDir;
+    private void OnEnable()
+    {
+        headerDir = Application.dataPath + "/HeaderTemplete.png";
+        printButton.onClick.AddListener(() => { PrintDocument(); });
+    }
+
+    void PrintDocument() {
+        smartPrinter.PrintDocument(inputField.text, headerDir);
+    }
+    // "C:\Users\f8704\OneDrive\Escritorio\Print unity\SmartPrinter\PSD Files\HeaderTemplete.png"
+}
